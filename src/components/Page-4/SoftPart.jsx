b838a98a9b703517b14403c96421e69f152e779e
angie <angyalva16@gmail.com>
2019-07-17 09:38:01 -0500

    added font family and added more soft kills

--- src/components/Page-4/SoftPart.jsx
+++ src/components/Page-4/SoftPart.jsx
@@ -9,39 +9,27 @@ class SoftKills extends React.Component{
                 <ul className="list-group list-group-flush bg-transparent">
                     <li className="list-group-item bg-transparent">
                         <h5>Liderazgo</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped  leadership" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Comunicación efectiva</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped communication" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Trabajo en equipo</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped teamWork" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Autoaprendizaje</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped selfLearning" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
-                    </li>
-                    <li className="list-group-item bg-transparent">
-                        <h5>Responsable</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped responsible" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Adaptable</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped adaptability" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
+                    </li>
+                    <li className="list-group-item bg-transparent">
+                        <h5>Resolución de problemas</h5>
+                    </li>
+                    <li className="list-group-item bg-transparent">
+                        <h5>Planeación</h5>
+                    </li>
+                    <li className="list-group-item bg-transparent">
+                        <h5>Manejo del tiempo</h5>
                     </li>
                 </ul>
             </div>
--- src/components/Page-4/TechPart.jsx
+++ src/components/Page-4/TechPart.jsx
@@ -9,51 +9,27 @@ class TechnicalSkills extends React.Component{
                 <ul className="list-group list-group-flush bg-transparent">
                     <li className="list-group-item bg-transparent">
                         <h5>Javascript Vanilla</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped js" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>HTML 5 & CSS 3</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped html-css" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>GitHub & Git</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped github" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>firebase</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped firebase" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Node.js</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped node-js" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Figma</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped figma" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>React JS</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped react-js" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                     <li className="list-group-item bg-transparent">
                         <h5>Bootstrap</h5>
-                        {/* <div className="progress">
-                            <div className="progress-bar progress-bar-striped bootstrap" role="progressbar"  aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-                        </div> */}
                     </li>
                 </ul>
             </div>
