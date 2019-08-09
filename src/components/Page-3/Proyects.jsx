0f63c56aee9821cdedd9dbc38a70d53255cd8365
angie <angyalva16@gmail.com>
2019-08-08 22:27:17 -0500

    Changed the carousel for cards in a proyects component

--- src/components/Page-3/Proyects.jsx
+++ src/components/Page-3/Proyects.jsx
@@ -1,82 +1,164 @@
 import React from "react";
 import "../styles/Proyects.css";
 
-class Proyects extends React.Component{
-    render(){
-        return(
-            <div className="bd-example" id="projects">
-                <br/><br/><br/><br/><br/>
-                <h1 className="title">Proyectos</h1>
-                <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-                    <ul className="carousel-indicators">
-                        <li data-target="#carouselExampleCaptions" data-slide-to="0" className="active"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-                        <li data-target="#carouselExampleCaptions" data-slide-to="3"></li>
-                    </ul>
-                    <div className="carousel-inner" >
-                        <div className="carousel-item active col-lg-12" data-interval="2000">
-                            <a target="_blank" href="https://shareazc.github.io/open-house-organa/#/" ><img src="https://fotos.subefotos.com/e99ef11b5288d8fb6226514902509bc7o.png" className="d-block w-100 img col-lg-6" alt="..."/></a>
-                            <div className="container col-md-6">
-                                <div className="card col-lg-12 card-color card-form d-md-block">
-                                    <h3 className="text-center">Organa</h3>
-                                    <h5 className="text-center">Proyecto creado para Laboratoria con tecnologías como React, React bootstrap, Firebase. Consiste en registrar la asistencia por medio de un código QR. Realicé éste proyecto en equipo.</h5>
-                                    <br/>
-                                    <br/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item col-lg-12" data-interval="2000">
-                            <a target="_blank" href="https://angieeca06.github.io/GDL002-burger-queen/#/" ><img src="https://fotos.subefotos.com/db20387c79ad521227d48addc60cfb11o.png" className="d-block col-lg-6 img w-100 " alt="..."/></a>
-                            <div className="container col-lg-6">
-                                <div className="card col-lg-12 card-color card-form  d-md-block">
-                                    <h3 className="text-center">Burger Queen</h3>
-                                    <h5 className="text-center">Proyecto creado para un restaurante con tecnologías como React, Bootstrap, Firebase. Consiste en una página donde el mesero puede tomar una orden y mandarla a la cocina, para ser preparadas.
-                                        <p className="instruction">
-                                        <i className="fas fa-exclamation-triangle iconWarning"></i>
-                                        (Para poder ingresar utiliza estos datos Email:angyalva16@gmail.com  Nombre: angie  No.empleado: 123456
-                                        </p>
-                                    </h5>
-                                    <br/>
-                                    <br/>
-                                </div>
-                            </div> 
-                        </div>
-                        <div className="carousel-item col-lg-12" data-interval="2000">
-                            <a target="_blank" href="https://angieeca06.github.io/gdl-2019-01-bc-core-cipher/src/" ><img src="https://fotos.subefotos.com/c7a3870b38c50a2857685f60f16b7326o.png" className="d-block col-lg-6 img w-100" alt="..."/></a>
-                            <div className="container col-lg-6">
-                                <div className="card col-lg-12 card-color card-form  d-md-block">
-                                    <h3 className="text-center">Cipher</h3>
-                                    <h5 className="text-center">Proyecto creado para cifrar y descifrar mensajes mediante el código Cesar Cipher, con tecnologías como vanillaJs, CSS 3, HTML 5.</h5>
-                                    <br/>
-                                    <br/>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="carousel-item col-lg-12" data-interval="2000">
-                            <a target="_blank" href="https://angieeca06.github.io/GDL002-framework-social-network/#/" ><img src="https://fotos.subefotos.com/1c449586547d64d7455684d7348f3bd8o.png" className="d-block col-lg-8 img w-100 image-fluid" title="Haz click para ver demo" alt="..."/></a>
-                            <div className="container col-lg-8">
-                                <div className="card col-lg-12 card-color card-form  d-md-block">
-                                    <h3 className="text-center">Red social</h3>
-                                    <h5 className="text-center">Proyecto creado para madres y padres de familia que pueden compartir tips y recomendaciones sobre sus hijos, con tecnologías como React, Firebase, Bootstrap.</h5>
-                                    <br/>
-                                    <br/>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <a className="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-                        <span className="carousel-control-prev-icon color" aria-hidden="true"></span>
-                        <span className="sr-only">Previous</span>
-                    </a>
-                    <a className="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-                        <span className="carousel-control-next-icon color" aria-hidden="true"></span>
-                        <span className="sr-only">Next</span>
-                    </a>
-                </div>
+class Proyects extends React.Component {
+  render() {
+    return (
+      <div className="bd-example" id="projects">
+        <br /><br /><br /><br /><br />
+        <h1 className="title">Proyectos</h1>
+        <div class="card mb-3 col-lg-8 col-md-8 col-sm-8 col-8 mx-auto bg-transparent border-0">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img
+                src="https://fotos.subefotos.com/e99ef11b5288d8fb6226514902509bc7o.png"
+                class="card-img img-fluid"
+                alt="..."
+              />
             </div>
-        )
-    }
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Organa</h5>
+                <p class="card-text">
+                  Proyecto creado para Laboratoria con tecnologías como React,
+                  React bootstrap, Firebase. Consiste en registrar la asistencia
+                  por medio de un código QR. Realicé éste proyecto en equipo.
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://shareazc.github.io/open-house-organa/#/"
+                  >
+                    Demo
+                  </a>
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://github.com/shareazc/open-house-organa"
+                  >
+                    Repositorio
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3 col-lg-8 col-md-8 col-sm-8 col-8 mx-auto bg-transparent border-0">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img
+                src="https://fotos.subefotos.com/db20387c79ad521227d48addc60cfb11o.png"
+                class="card-img img-fluid"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Burguer Queen</h5>
+                <p class="card-text">
+                  Proyecto creado para un restaurante con tecnologías como
+                  React, Bootstrap, Firebase. Consiste en una página donde el
+                  mesero puede tomar una orden y mandarla a la cocina, para ser
+                  preparadas.
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://angieeca06.github.io/GDL002-burger-queen/#/"
+                  >
+                    Demo
+                  </a>
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://github.com/angieeca06/GDL002-burger-queen"
+                  >
+                    Repositorio
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3 col-lg-8 col-md-8 col-sm-8 col-8 mx-auto bg-transparent border-0">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img
+                src="https://fotos.subefotos.com/c7a3870b38c50a2857685f60f16b7326o.png"
+                class="card-img img-fluid"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Cipher</h5>
+                <p class="card-text">
+                  Proyecto creado para cifrar y descifrar mensajes mediante el
+                  código Cesar Cipher, con tecnologías como vanillaJs, CSS 3,
+                  HTML 5.
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://angieeca06.github.io/gdl-2019-01-bc-core-cipher/src/"
+                  >
+                    Demo
+                  </a>
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://github.com/angieeca06/gdl-2019-01-bc-core-cipher"
+                  >
+                    Repositorio
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3 col-lg-8 col-md-8 col-sm-8 col-8 mx-auto bg-transparent border-0">
+          <div class="row no-gutters">
+            <div class="col-md-4">
+              <img
+                src="https://fotos.subefotos.com/1c449586547d64d7455684d7348f3bd8o.png"
+                class="card-img img-fluid"
+                alt="..."
+              />
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Parentips</h5>
+                <p class="card-text">
+                  Proyecto creado para madres y padres de familia que pueden
+                  compartir tips y recomendaciones sobre sus hijos, con
+                  tecnologías como React, Firebase, Bootstrap.
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://angieeca06.github.io/GDL002-framework-social-network/#/"
+                  >
+                    Demo
+                  </a>
+                </p>
+                <p class="card-text">
+                  <a
+                    target="_blank"
+                    href="https://github.com/angieeca06/GDL002-framework-social-network"
+                  >
+                    Repositorio
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Proyects;
--- src/components/Page-5/Contact.jsx
+++ src/components/Page-5/Contact.jsx
@@ -1,38 +1,61 @@
 import React from "react";
-import "../styles/Contact.css"
+import "../styles/Contact.css";
 
-class FormContact extends React.Component{
-    render(){
-        return(
-            <div id="contact">
-                <br/><br/><br/><br/><br/>
-                <h1 className="title">Contacto</h1>
-                <div className="container col-lg-10 space" >
-                    <h1 className="text-center email">Si quieres contactarte conmigo mandame un email  </h1>
-                    <h2 className="text-decoration text-center email">angyalva16@gmail.com</h2>
-                    <br/><br/><br/>
-                    <div className="row">
-                        <div className="col col-lg-4">
-                            <a className="bg-transparent style-btn no-gutters " target="_blank" href="https://github.com/angieeca06">
-                                <img src="https://image.flaticon.com/icons/png/512/25/25657.png" className="img-fluid contact rounded mx-auto d-block" alt="..." />
-                                <br/>
-                                <h1 className="text-center text">GitHub</h1>
-                            </a>
-                            
-                        </div>
-                        <div className="col col-lg-4">
-                            <a className="bg-transparent style-btn no-gutters " target="_blank" href="https://www.linkedin.com/in/ang%C3%A9lica-caballero/">
-                                <img src="https://image.flaticon.com/icons/png/512/61/61109.png" className="img-fluid contact rounded mx-auto d-block" alt="..." />
-                                <br/>
-                                <h1 className="text-center text">Linkedin</h1>
-                            </a>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>       
-        )
-    }
+class FormContact extends React.Component {
+  render() {
+    return (
+      <div id="contact">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <h1 className="title">Contacto</h1>
+        <div className="container col-lg-10 space">
+          <a href="mailto:angyalva16@gmail.com" target="_top">
+            <h1 className="text-center email text-muted text-decoration-none">
+              Si quieres contactarte conmigo mandame un email
+            </h1>
+          </a>
+          <br />
+          <br />
+          <br />
+          <div className="row">
+            <div className="col col-lg-4">
+              <a
+                className="bg-transparent style-btn no-gutters "
+                target="_blank"
+                href="https://github.com/angieeca06"
+              >
+                <img
+                  src="https://image.flaticon.com/icons/png/512/25/25657.png"
+                  className="img-fluid contact rounded mx-auto d-block"
+                  alt="..."
+                />
+                <br />
+                <h1 className="text-center text">GitHub</h1>
+              </a>
+            </div>
+            <div className="col col-lg-4">
+              <a
+                className="bg-transparent style-btn no-gutters "
+                target="_blank"
+                href="https://www.linkedin.com/in/ang%C3%A9lica-caballero/"
+              >
+                <img
+                  src="https://image.flaticon.com/icons/png/512/61/61109.png"
+                  className="img-fluid contact rounded mx-auto d-block"
+                  alt="..."
+                />
+                <br />
+                <h1 className="text-center text">Linkedin</h1>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default FormContact;
